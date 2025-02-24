fe657f2010924297468e527a4a0fb3f2962c3afa
Chris <chrisbutler296@gmail.com>
2025-02-23 20:04:30 -0700

    Init

---
+++ fetch_tinysys_sdk.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+function do_loudly() {
+    echo "+ $*"
+    $*
+}
+
+set -e
+
+
+mkdir -pv tinsys_c_sdk
+OUTDIR=$(realpath tinsys_c_sdk)
+
+REPO=target/tinysys_repo
+if [ ! -d "$REPO" ]; then
+    do_loudly git clone https://github.com/ecilasun/tinysys.git \
+        --no-checkout $REPO                           \
+        --depth       1
+    pushd $REPO > /dev/null
+
+    pwd
+    do_loudly git sparse-checkout init --cone
+    do_loudly git sparse-checkout set software/SDK/
+    do_loudly git checkout
+else
+    echo "Found $REPO, attempting to update"
+    pushd $REPO > /dev/null
+    do_loudly git fetch
+fi
+
+echo $(git rev-parse HEAD)      > $OUTDIR/git-HEAD.txt
+echo                           >> $OUTDIR/git-HEAD.txt
+echo $(git log --oneline HEAD) >> $OUTDIR/git-HEAD.txt
+
+popd > /dev/null
+
+file $REPO/software/SDK
+file $OUTDIR
+do_loudly cp -rv $REPO/software/SDK $OUTDIR/
---
+++ src/lib.rs
@@ -0,0 +1,3 @@
+#![cfg_attr(not(any(test, feature="std")), no_std)]
+
+pub struct Api;
