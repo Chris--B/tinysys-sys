502d51f1fc66300e90d6cf3c739060951ed2b7e0
Chris <chrisbutler296@gmail.com>
2025-03-23 16:50:48 -0600

    Update build.rs
    u128 doesn't behave well over FFI and generates warnings for its use.
    The only things using 128-bit ints are libc math functions, which don't
    need to be exported here at all. This change creates a block list and we
    use it to make sure these math functions are not included.
    
    We want to keep the "not FFI safe" warning enabled in case it catches a
    real issue.

--- build.rs
+++ build.rs
@@ -16,8 +16,10 @@ fn do_bindgen() {
         println!("cargo:rerun-if-changed={}", file);
     }
 
-    // TODO: We use this to set the sysroot when cross compiling, but could probably do that logic here
+    // TODO: We use this to set the sysroot when cross compiling (from update_rs.sh), but could probably do that logic here
     println!("cargo:rerun-if-env-changed={}", "BINDGEN_EXTRA_CLANG_ARGS");
+
+    let blocklist_c_funcs_regex = format!("({})", BLOCKLIST_C_FUNCS.join("|"));
 
     let bindings = bindgen::Builder::default()
         .header("src/include/wrapper.h")
@@ -28,8 +30,13 @@ fn do_bindgen() {
         // items needed by a function definition. This eliminates >80% of
         // the symbols typically found.
         .allowlist_function(".*")
+        .blocklist_function(blocklist_c_funcs_regex)
         .merge_extern_blocks(true)
         .generate()
+        // Note: If you're seeing this crash, make sure you're using update_rs.sh or otherwise telling bindgen uses your cross compiler.
+        // It doesn't by default and likes to crash in **host** C headers with nonsense like:
+        //      thread 'main' panicked at build.rs:40:10:
+        //      Unable to generate bindings: ClangDiagnostic("/Library/Developer/CommandLineTools/usr/lib/clang/16/include/inttypes.h:21:15: fatal error: 'inttypes.h' file not found\n")
         .expect("Unable to generate bindings");
 
     let mut code = vec![];
@@ -50,3 +57,70 @@ fn do_bindgen() {
         std::fs::write("src/sdk.rs", code).unwrap();
     }
 }
+
+// Note: This is unused when bindgen is not enabled (which is most of the time)
+#[allow(unused)]
+const BLOCKLIST_C_FUNCS: &[&str] = &[
+    // Block all math functions that use 128 bit ints. u128 isn't well behaved over FFI,
+    // and we don't want to export a bunch of libc functions anyway.
+    "acoshl",
+    "acosl",
+    "asinhl",
+    "asinl",
+    "atan2l",
+    "atanhl",
+    "atanl",
+    "cbrtl",
+    "ceill",
+    "copysignl",
+    "coshl",
+    "cosl",
+    "erfcl",
+    "erfl",
+    "exp2l",
+    "expl",
+    "expm1l",
+    "fabsl",
+    "fdiml",
+    "finitel",
+    "floorl",
+    "fmal",
+    "fmaxl",
+    "fminl",
+    "fmodl",
+    "frexpl",
+    "hypotl",
+    "ilogbl",
+    "ldexpl",
+    "lgammal",
+    "llrintl",
+    "llroundl",
+    "log10l",
+    "log1pl",
+    "log2l",
+    "logbl",
+    "logl",
+    "lrintl",
+    "lroundl",
+    "modfl",
+    "nanl",
+    "nearbyintl",
+    "nextafterl",
+    "nexttoward",
+    "nexttowardf",
+    "nexttowardl",
+    "powl",
+    "remainderl",
+    "remquol",
+    "rintl",
+    "roundl",
+    "scalblnl",
+    "scalbnl",
+    "sinhl",
+    "sinl",
+    "sqrtl",
+    "tanhl",
+    "tanl",
+    "tgammal",
+    "truncl",
+];
--- src/lib.rs
+++ src/lib.rs
@@ -5,3 +5,7 @@
 mod sdk;
 #[cfg(target_arch = "riscv32")]
 pub use sdk::*;
+
+/// This crate is largely generated and from a specific git rev of the tinysys SDK headers.
+/// This is the git rev that was used. See the full tinysys repo for more information on this specific hash.
+pub const TINYSYS_SDK_GIT_REV: &str = include_str!("../tinysys_c_sdk/git-HEAD.txt");
