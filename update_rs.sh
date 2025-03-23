326875df327a070bfb40eb088f600b4863f574cf
Chris <chrisbutler296@gmail.com>
2025-03-23 16:26:31 -0600

    Make `update_rs.sh` fail if there's not riscv gcc
    Printing the version like this will immediate error out if the user
    doesn't have riscv tools setup correctly.

--- update_rs.sh
+++ update_rs.sh
@@ -34,6 +34,7 @@ echo "+ Done"
 #     --std=c++17
 
 # Our build script generates the rs files
+riscv64-unknown-elf-gcc --version
 export BINDGEN_EXTRA_CLANG_ARGS="--sysroot=$(riscv64-unknown-elf-gcc -print-sysroot)"
 do_loudly cargo build --features=bindgen $*
 do_loudly cargo fmt
